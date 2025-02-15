9d004db326e39fbe71ae1b7babdde933e6561a08
TalhaJamal02 <chrislandson057@gmail.com>
2025-02-15 18:16:59 +0500

    Day-1

---
+++ day-1-2/first.py
@@ -0,0 +1,41 @@
+import math
+
+print("Hello, World!")
+
+name: str = "Talha Jamal"
+age: int = 17
+
+print("My name is", name, "and I am", age, "years old.")
+
+a: int = 6
+b: int = 4
+
+# addition
+print(a + b)
+# subtraction
+print(a - b)
+# multiplication
+print(a * b)
+# division
+print(a / b)
+# floor division (Divides and rounds down to the nearest integer.)
+print(a // b)
+# modulus (Returns the remainder of a division.) 
+print(a % b)
+# power (Raises a to the power of b.)
+print(a ** b)
+
+# square root
+print(int(math.sqrt(b)))
+# power
+print(math.pow(a, b))
+
+c: int = -3
+# absolute value (returns the absolute value of a number, meaning it removes any negative sign and returns only the magnitude.)
+print(abs(c))
+
+d: float = 3.5
+# round (rounds a number to the nearest integer.)
+print(round(d))
+
+
